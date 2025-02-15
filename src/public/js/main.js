7936a6236b4ced959f6832815c854b538a12b8ce
AlphaT7 <7742643+AlphaT7@users.noreply.github.com>
2025-02-15 16:37:15 -0500

    more interface integration

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,10 +1,20 @@
+import AudioInput from "./audioAnalysis.js";
+
 const $ = document.querySelector.bind(document);
-const terminal = new Terminal();
-terminal.open($("#terminal1"));
-terminal.write("Just push the button and talk! =)");
+const audioAnalysis = new AudioInput();
+
+// terminal.open($("#terminal1"));
+// terminal.write("Just push the button and talk! =)");
 
 $("#button").addEventListener("pointerup", (el) => {
+  if (audioAnalysis.audioListening) return;
+
+  $("#listening").style.color = "#19ab33";
+  $("#listening").innerHTML = "Listening...";
+  $("#button").style.color = "#19ab33";
   $("#button").classList.add("box-ripple");
+
+  audioAnalysis.start();
   setTimeout(() => {
     $("#button").classList.remove("box-ripple");
   }, 900);
